d3e69ea6e616e899eedcc57e6a9d32dfa1df0a67
oksdud <oksana.dudnik@lpb.lv>
2017-06-02 12:48:14 +0300

    update program

--- src/main/java/com/adonis/ui/main/MainScreen.java
+++ src/main/java/com/adonis/ui/main/MainScreen.java
@@ -9,7 +9,6 @@ import com.adonis.ui.persons.PersonsCrudView;
 import com.adonis.ui.persons.RegistrationUI;
 import com.adonis.ui.print.PrintView;
 import com.adonis.ui.renta.RentaChartView;
-import com.adonis.ui.renta.RentaDChartView;
 import com.adonis.ui.renta.RentaHistoryCrudView;
 import com.adonis.ui.vehicles.VehicleModelsCrudView;
 import com.adonis.ui.vehicles.VehicleTypesCrudView;
@@ -58,7 +57,7 @@ public class MainScreen extends HorizontalLayout implements View {
         menu.addView(ui.getPersonsCrudView(), PersonsCrudView.NAME, PersonsCrudView.NAME, new ThemeResource("img/customers.jpg"));
         menu.addView(new RentaHistoryCrudView(ui.rentaHistoryService,ui.service, ui.vehicleService), RentaHistoryCrudView.NAME, RentaHistoryCrudView.NAME, new ThemeResource("img/for-rent.jpg"));
         menu.addView(new RentaChartView(ui.service, ui.rentaHistoryService, ui.vehicleService), RentaChartView.NAME, RentaChartView.NAME, new ThemeResource("img/chart.jpg"));
-        menu.addView(new RentaDChartView(ui.service, ui.rentaHistoryService, ui.vehicleService), RentaDChartView.NAME, RentaDChartView.NAME, new ThemeResource("img/barChart.png"));
+//        menu.addView(new RentaDChartView(ui.service, ui.rentaHistoryService, ui.vehicleService), RentaDChartView.NAME, RentaDChartView.NAME, new ThemeResource("img/barChart.png"));
         menu.addView(new RegistrationUI(ui.service), "CUSTOMER REGISTRATION" , "CUSTOMER REGISTRATION", new ThemeResource("img/Register-Today.jpg"));
         menu.addView(new PersonUI(ui.service, true, MainUI.loginPerson), "PROFILE" , "PROFILE", new ThemeResource("img/user-icon.jpg"));
         menu.addView(new PrintView(ui.service, ui.rentaHistoryService), "PRINT", "PRINT", new ThemeResource("img/print-icon.jpg"));
