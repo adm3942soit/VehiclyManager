90a88ee411ab600bfa811d1405ab6dadc81cad24
oksdud <oksana.dudnik@lpb.lv>
2018-04-13 09:08:58 +0300

    update editable image

--- run.bat
+++ run.bat
@@ -1,8 +1,2 @@
-#!/bin/sh
-#
-# ---------------------------------------------------------------------
-# VehiclyManager startup script.
-# ---------------------------------------------------------------------
-#
-cd C:\MyLibs\VehiclyManager
+cd C:\MyLibs\Vaadin\VehiclyManager
 java -jar build/libs/VehiclyManager-1.0.0.jar
